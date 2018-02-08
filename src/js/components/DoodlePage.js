219e70c6fee29ca52a10376589b599e5b2a734e7
Dustin L. Dodson <dustinleedodson@gmail.com>
2018-02-07 19:46:23 -0900

    Added DoodlePage component to embed doodle polls into the website.

---
+++ src/js/components/DoodlePage.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Grid, Row, Col} from 'react-bootstrap';
+
+class DoodlePage extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	componentWillMount() {
+		
+	}
+
+	componentDidMount() {
+
+	}
+
+	render() {
+		return (
+			<Grid>
+				<Row className="show-grid">
+					<Col xs={2} sm={2} md={2} lg={2}></Col>
+					<Col xs={8} sm={8} md={8} lg={8}>
+						<div id="doodle-poll-container">
+							<iframe id="doodle-poll" 
+								className="text-center"
+								src={`https://doodle.com/poll/${this.props.pollId}`}>
+							</iframe>
+						</div>
+					</Col>
+					<Col xs={2} sm={2} md={2} lg={2}></Col>
+				</Row>
+			</Grid>
+		);
+	}
+}
+
+export default DoodlePage;
