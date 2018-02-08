84cbaf1b498446ffae6eec579241b1c7bfd46ef8
Jason Warta <jasonwarta@gmail.com>
2018-02-07 18:56:17 -0900

    added navbar and updated autogen files

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Route, Redirect, Switch } from 'react-router-dom';
+
 import PlaceHolder from './components/PlaceHolder';
+import NoMatch from './components/NoMatch';
+import Navbar from './components/Navbar';
 
-const app = document.getElementById('app');
+const ROUTES = [
+	{
+		'title':'Home',
+		'href':'/home',
+		'component': (props) => {
+			return (
+				<PlaceHolder {...props} />
+			)
+		}
+	},
+	{
+		'title':'About',
+		'href':'/about',
+		'component': (props) => {
+			return (
+				<PlaceHolder {...props} />
+			)
+		}
+	},
+	{
+		'title':'Contact',
+		'href':'/contact',
+		'component': (props) => {
+			return (
+				<PlaceHolder {...props} />
+			)
+		}
+	}
+];
 
-ReactDOM.render(<PlaceHolder/>, app);
+class App extends React.Component {
+	render() {
+		return (
+			<HashRouter>
+				<div>
+					<Navbar links={ROUTES} />
+
+					<div className='container'>
+					<Switch>
+						{ROUTES.map( (link,i) => {
+							return (
+								<Route 
+									path={link.href} 
+									component={link.component} />
+							);
+						})}
+						
+						<Redirect exact from="/" to="/home" />
+						<Route component={NoMatch} />
+					</Switch>
+					</div>
+				</div>
+			</HashRouter>
+		);
+	}
+}
+
+
+
+ReactDOM.render(
+	<App />, 
+	document.getElementById('app') 
+);
