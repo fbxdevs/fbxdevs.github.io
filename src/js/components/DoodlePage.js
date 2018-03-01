397170fe764c8de13434537c16f8b34f0de27828
Jason Warta <jasonwarta@gmail.com>
2018-02-28 16:35:17 -0900

    changed up some css on the doodle page to maybe fix mobile view

--- src/js/components/DoodlePage.js
+++ src/js/components/DoodlePage.js
@@ -1,5 +1,5 @@
 import {Component} from 'react';
-import {Grid, Row, Col} from 'react-bootstrap';
+// import {Grid, Row, Col} from 'react-bootstrap';
 
 class DoodlePage extends Component {
 	constructor(props) {
@@ -16,20 +16,14 @@ class DoodlePage extends Component {
 
 	render() {
 		return (
-			<Grid>
-				<Row className="show-grid">
-					<Col xs={2} sm={2} md={2} lg={2}></Col>
-					<Col xs={8} sm={8} md={8} lg={8}>
-						<div id="doodle-poll-container">
-							<iframe id="doodle-poll"
-								className="text-center"
-								src={`https://doodle.com/poll/${this.props.pollId}`}>
-							</iframe>
-						</div>
-					</Col>
-					<Col xs={2} sm={2} md={2} lg={2}></Col>
-				</Row>
-			</Grid>
+			
+			<div id="doodle-poll-container">
+				<iframe id="doodle-poll"
+					className="text-center"
+					src={`https://doodle.com/poll/${this.props.pollId}`}>
+				</iframe>
+			</div>
+					
 		);
 	}
 }
